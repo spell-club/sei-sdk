0567f271b9b629fc4a2c1b4798de0c8b8ab2ee63
egor.oveian <egor.oveian@hiveon.net>
2024-05-27 11:01:42 +0100

    add instantitate func

--- client_instantiate.go
+++ client_instantiate.go
@@ -1,6 +1,30 @@
 package seisdk
 
-func (c *Client) Instantiate(address, code string) (string, error) {
+import (
+	"errors"
+	"fmt"
 
-	return "", nil
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+)
+
+func (c *Client) Instantiate(address string, code string) (string, error) {
+	if code == "" {
+		return "", errors.New("message is empty")
+	}
+
+	message := &wasmtypes.MsgInstantiateContract{
+		Sender: address,
+		Msg:    []byte(code),
+	}
+
+	txResult, err := c.asyncBroadcastMsg(message)
+	if err != nil {
+		return "", fmt.Errorf("AsyncBroadcastMsg: %s", err)
+	}
+
+	if txResult == nil || txResult.GetTxResponse() == nil {
+		return "", fmt.Errorf("txResult is nil: %v", txResult)
+	}
+
+	return txResult.GetTxResponse().TxHash, nil
 }
