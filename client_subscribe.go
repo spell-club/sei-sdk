571b28882a8e78908112be28bd038dd9b17bb933
egor.oveian <egor.oveian@hiveon.net>
2024-05-30 20:19:26 +0100

    move pure ws connection to tendermint client

--- client_subscribe.go
+++ client_subscribe.go
@@ -1,102 +1,52 @@
 package sdk
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"sync"
-	"time"
 
-	"github.com/gorilla/websocket"
+	abci "github.com/tendermint/tendermint/abci/types"
 )
 
-const reconnectDelay = 2 * time.Second
+var subscribeQuery = "tm.event='Tx' AND wasm._contract_address CONTAINS '%s' AND wasm.action='execute_claim'"
 
-var subscribeMessage = `{
-	"jsonrpc": "2.0",
-	"method": "subscribe",
-	"id": 0,
-	"params": {
-		"query": "tm.event='Tx' AND wasm._contract_address CONTAINS '%s' AND wasm.action='execute_claim'"
-	}
-}`
-
-var unsubscribeMessage = `{
-	"jsonrpc": "2.0",
-	"method": "unsubscribe",
-	"id": 0,
-}`
-
-func (c *Client) Subscribe(ctx context.Context, contractAddress string, acknowledge func(ctx context.Context, msg Events) error) error {
+func (c *Client) Subscribe(ctx context.Context, contractAddress string, acknowledge func(ctx context.Context, msg []abci.Event) error) error {
 	if c.wss.host == "" {
 		return fmt.Errorf("websocket host wasn't provided in config")
 	}
 
-	for {
-		conn, _, err := websocket.DefaultDialer.Dial(c.wss.host, nil)
+	tendermintNode, err := c.sign.ctx.GetNode()
+	if err != nil {
+		return fmt.Errorf("ctx.GetNode(): %v", err)
+	}
+
+	eventsChan, err := tendermintNode.Subscribe(ctx, "", fmt.Sprintf(subscribeQuery, contractAddress))
+	if err != nil {
+		return fmt.Errorf("tendermintNode.Subscribe: %v", err)
+	}
+
+	wg := &sync.WaitGroup{}
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		for event := range eventsChan {
+			err = acknowledge(ctx, event.Events)
+			if err != nil {
+				c.logger.Logger.Errorf("conn.ReadMessage: %s", err)
+
+				return
+			}
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		err = tendermintNode.UnsubscribeAll(ctx, "")
 		if err != nil {
-			return fmt.Errorf("DefaultDialer.Dial: %w", err)
+			return fmt.Errorf("tendermintNode.UnsubscribeAll: %v", err)
 		}
+		wg.Wait()
 
-		err = conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf(subscribeMessage, contractAddress)))
-		if err != nil {
-			return fmt.Errorf("conn.WriteMessage: %w", err)
-		}
-
-		wg := &sync.WaitGroup{}
-		done := make(chan struct{})
-		go func() {
-			wg.Add(1)
-			defer wg.Done()
-
-			for {
-				var message []byte
-				_, message, err = conn.ReadMessage()
-				if err != nil {
-					c.logger.Logger.Errorf("conn.ReadMessage: %s", err)
-
-					return
-				}
-
-				resp := SubscribeMessage{}
-				err = json.NewDecoder(bytes.NewReader(message)).Decode(&resp)
-				if err != nil {
-					c.logger.Logger.Errorf("Decode: %s", err)
-
-					return
-				}
-
-				err = acknowledge(ctx, resp.Result.Events)
-				if err != nil {
-					c.logger.Logger.Errorf("acknowledge: %s", err)
-
-					return
-				}
-			}
-		}()
-
-		select {
-		case <-done:
-			c.logger.Warnf("Connection lost, attempting to reconnect...")
-			err = conn.Close()
-			if err != nil {
-				return fmt.Errorf("conn.Close: %w", err)
-			}
-			time.Sleep(reconnectDelay)
-		case <-ctx.Done():
-			c.logger.Warnf("Interrupt received, closing connection...")
-			err = conn.WriteMessage(websocket.TextMessage, []byte(unsubscribeMessage))
-			if err != nil {
-				return fmt.Errorf("conn.WriteMessage: %w", err)
-			}
-			err = conn.Close()
-			if err != nil {
-				return fmt.Errorf("conn.Close: %w", err)
-			}
-			wg.Wait()
-
-			return nil
-		}
+		return nil
 	}
 }
