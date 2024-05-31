be758b6f46a95e11233aa1d2d87fafaf86e69960
egor.oveian <egor.oveian@hiveon.net>
2024-05-31 01:22:37 +0100

    add additional steps to handle txs function

--- client_handle_tx_by_height.go
+++ client_handle_tx_by_height.go
@@ -2,12 +2,14 @@ package sdk
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
+	"strconv"
 
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
-const searchByHeightQuery = `tx.height>%d AND tx.height<%d AND wasm._contract_address CONTAINS '%s' AND wasm.action='execute_claim'`
+const searchByHeightQuery = `tx.height>%d AND tx.height<%d AND wasm._contract_address CONTAINS '%s'`
 
 func (c *Client) HandleTxsByHeight(ctx context.Context, contractAddress string, heightFrom, heightTo int64, acknowledge func(ctx context.Context, msg []abci.Event) error) error {
 	query := fmt.Sprintf(searchByHeightQuery, heightFrom, heightTo, contractAddress)
@@ -26,6 +28,29 @@ func (c *Client) HandleTxsByHeight(ctx context.Context, contractAddress string, 
 		}
 
 		for i := range resp.Txs {
+			// Create tx_hash event
+			txHashEvent := abci.Event{
+				Type: "tx",
+				Attributes: []abci.EventAttribute{
+					{
+						Key:   []byte("hash"),
+						Value: []byte(hex.EncodeToString(resp.Txs[i].Hash)),
+					},
+				},
+			}
+
+			// Create tx_height event
+			txHeightEvent := abci.Event{
+				Type: "tx",
+				Attributes: []abci.EventAttribute{
+					{
+						Key:   []byte("height"),
+						Value: []byte(strconv.FormatInt(resp.Txs[i].Height, 10)),
+					},
+				},
+			}
+
+			resp.Txs[i].TxResult.Events = append(resp.Txs[i].TxResult.Events, []abci.Event{txHeightEvent, txHashEvent}...)
 			err = acknowledge(ctx, resp.Txs[i].TxResult.Events)
 			if err != nil {
 				return err
