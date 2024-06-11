deeb3fe586f3404ce8aa215d4610ec53f69c262d
Maksym Koval <m.koval@everstake.one>
2024-06-11 10:13:45 +0300

    split TxSearch on ranges

--- client_handle_tx_by_height.go
+++ client_handle_tx_by_height.go
@@ -8,60 +8,71 @@ import (
 	"strconv"
 )
 
-const searchByHeightQuery = `tx.height>%d AND tx.height<%d AND wasm._contract_address CONTAINS '%s'`
+const (
+	searchByHeightQuery = `tx.height>%d AND tx.height<=%d AND wasm._contract_address CONTAINS '%s'`
+	rangeSize           = 100_000
+)
 
 func (c *Client) HandleTxsByHeight(ctx context.Context, contractAddress string, heightFrom, heightTo int64, acknowledge func(ctx context.Context, msg []abci.Event) error) error {
-	query := fmt.Sprintf(searchByHeightQuery, heightFrom, heightTo, contractAddress)
 	tendermintNode, err := c.clientCtx.GetNode()
 	if err != nil {
-		return err
+		return fmt.Errorf("clientCtx.GetNode: %w", err)
 	}
 
-	page := 1
 	txsCount := 100
+	from := heightFrom
+	to := min(heightFrom+rangeSize, heightTo)
 
 	for {
-		resp, err := tendermintNode.TxSearch(ctx, query, true, &page, &txsCount, "desc")
-		if err != nil {
-			return fmt.Errorf("tendermintNode.TxSearch: %w", err)
-		}
+		page := 1
+		query := fmt.Sprintf(searchByHeightQuery, from, to, contractAddress)
 
-		for i := range resp.Txs {
-			// Create tx_hash event
-			txHashEvent := abci.Event{
-				Type: "tx",
-				Attributes: []abci.EventAttribute{
-					{
-						Key:   []byte("hash"),
-						Value: []byte(hex.EncodeToString(resp.Txs[i].Hash)),
-					},
-				},
-			}
-
-			// Create tx_height event
-			txHeightEvent := abci.Event{
-				Type: "tx",
-				Attributes: []abci.EventAttribute{
-					{
-						Key:   []byte("height"),
-						Value: []byte(strconv.FormatInt(resp.Txs[i].Height, 10)),
-					},
-				},
-			}
-
-			resp.Txs[i].TxResult.Events = append(resp.Txs[i].TxResult.Events, []abci.Event{txHeightEvent, txHashEvent}...)
-			err = acknowledge(ctx, resp.Txs[i].TxResult.Events)
+		for {
+			resp, err := tendermintNode.TxSearch(ctx, query, true, &page, &txsCount, "asc")
 			if err != nil {
-				return err
+				return fmt.Errorf("tendermintNode.TxSearch: %w", err)
 			}
-		}
 
-		if len(resp.Txs) < 100 {
-			break
-		}
+			for i := range resp.Txs {
+				// Create tx_hash event
+				txHashEvent := abci.Event{
+					Type: "tx",
+					Attributes: []abci.EventAttribute{
+						{
+							Key:   []byte("hash"),
+							Value: []byte(hex.EncodeToString(resp.Txs[i].Hash)),
+						},
+					},
+				}
 
-		page++
+				// Create tx_height event
+				txHeightEvent := abci.Event{
+					Type: "tx",
+					Attributes: []abci.EventAttribute{
+						{
+							Key:   []byte("height"),
+							Value: []byte(strconv.FormatInt(resp.Txs[i].Height, 10)),
+						},
+					},
+				}
+
+				resp.Txs[i].TxResult.Events = append(resp.Txs[i].TxResult.Events, []abci.Event{txHeightEvent, txHashEvent}...)
+				err = acknowledge(ctx, resp.Txs[i].TxResult.Events)
+				if err != nil {
+					return err
+				}
+			}
+
+			if len(resp.Txs) < 100 {
+				if to == heightTo {
+					return nil
+				}
+				from = to
+				to = min(from+rangeSize, heightTo)
+				break
+			}
+
+			page++
+		}
 	}
-
-	return nil
 }
