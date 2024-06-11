f53014ffa9d20ad6350fe09ec1faf792944630f1
Maksym Koval <m.koval@everstake.one>
2024-06-11 16:55:34 +0300

    fmt

--- methods.go
+++ methods.go
@@ -6,6 +6,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"strconv"
 	"strings"
 	"time"
 
@@ -19,6 +21,9 @@ import (
 const (
 	// DefaultDenom is the default denomination for Sei blockchain
 	DefaultDenom = "usei"
+
+	searchByHeightQuery = `tx.height>%d AND tx.height<=%d AND wasm._contract_address CONTAINS '%s'`
+	rangeSize           = 100_000
 )
 
 // GetBankBalance queries a Cosmos SDK bank for the balance of a specific account denominated in a specific denom
@@ -168,6 +173,21 @@ func (c *Client) GetTxByHash(ctx context.Context, txHash string, retries uint, s
 	return
 }
 
+// GetLatestHeight retrieves latest height from the network.
+func (c *Client) GetLatestHeight(ctx context.Context) (int64, error) {
+	tendermintNode, err := c.clientCtx.GetNode()
+	if err != nil {
+		return 0, err
+	}
+
+	resp, err := tendermintNode.Status(ctx)
+	if err != nil {
+		return 0, fmt.Errorf("tendermintNode.Status: %w", err)
+	}
+
+	return resp.SyncInfo.LatestBlockHeight, nil
+}
+
 // GetTxMetaResponseByHash retrieves transaction metadata response from the network. retries and sleepInterval params can be used to re-retrieve tx in case of error
 func (c *Client) GetTxMetaResponseByHash(ctx context.Context, txHash string, retries uint, sleepInterval time.Duration) (txResp *txtypes.GetTxResponse, err error) {
 	// first is request, after n retries
@@ -212,4 +232,69 @@ func (c *Client) GetTxMetaResponseByHash(ctx context.Context, txHash string, ret
 	}
 
 	return
+}
+
+// HandleTxsByHeight retrieves contract transaction by height and process via callback.
+func (c *Client) HandleTxsByHeight(ctx context.Context, contractAddress string, heightFrom, heightTo int64, acknowledge func(ctx context.Context, msg []abci.Event) error) error {
+	tendermintNode, err := c.clientCtx.GetNode()
+	if err != nil {
+		return fmt.Errorf("clientCtx.GetNode: %w", err)
+	}
+
+	txsCount := 100
+	from := heightFrom
+	to := min(heightFrom+rangeSize, heightTo)
+
+	for {
+		page := 1
+		query := fmt.Sprintf(searchByHeightQuery, from, to, contractAddress)
+
+		for {
+			resp, err := tendermintNode.TxSearch(ctx, query, true, &page, &txsCount, "asc")
+			if err != nil {
+				return fmt.Errorf("tendermintNode.TxSearch: %w", err)
+			}
+
+			for i := range resp.Txs {
+				// Create tx_hash event
+				txHashEvent := abci.Event{
+					Type: "tx",
+					Attributes: []abci.EventAttribute{
+						{
+							Key:   []byte("hash"),
+							Value: []byte(hex.EncodeToString(resp.Txs[i].Hash)),
+						},
+					},
+				}
+
+				// Create tx_height event
+				txHeightEvent := abci.Event{
+					Type: "tx",
+					Attributes: []abci.EventAttribute{
+						{
+							Key:   []byte("height"),
+							Value: []byte(strconv.FormatInt(resp.Txs[i].Height, 10)),
+						},
+					},
+				}
+
+				resp.Txs[i].TxResult.Events = append(resp.Txs[i].TxResult.Events, []abci.Event{txHeightEvent, txHashEvent}...)
+				err = acknowledge(ctx, resp.Txs[i].TxResult.Events)
+				if err != nil {
+					return err
+				}
+			}
+
+			if len(resp.Txs) < 100 {
+				if to == heightTo {
+					return nil
+				}
+				from = to
+				to = min(from+rangeSize, heightTo)
+				break
+			}
+
+			page++
+		}
+	}
 }
